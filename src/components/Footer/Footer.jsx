1dbc1c50c8c02f42256ec031ec255c533897e238
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2024-11-07 16:15:38 -0300

    creation of main pages

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Blog React & Firebase &copy; {new Date().getFullYear()}</p>
+      <p>
+        Desenvolvido por{" "}
+        <a href="https://www.linkedin.com/in/jose-gui/" target="_blank">
+          José Guilherme Mouta Ferreira
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
