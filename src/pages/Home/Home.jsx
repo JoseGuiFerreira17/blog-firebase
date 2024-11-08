dda80d8323c3c04ec4950ad12afc1522355415fc
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2024-11-08 15:51:09 -0300

    home page creation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,44 @@
 import styles from "./Home.module.css";
 
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+import PostDetail from "../../components/PostDetail/PostDetail";
+
 function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const [search, setSearch] = useState("");
+  const { documents: posts, loading } = useFetchDocuments("posts");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.home}>
+      <h1>Veja nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail post={post} key={post.id} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <h2>Nenhum post encontrado</h2>
+            <Link to="/create-post" className="btn">
+              Crie um post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
